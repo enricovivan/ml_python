d791da39623722146d8b2e9f1dd91a6186ab2df3
enricovivan <enricovivan21@gmail.com>
2023-01-19 17:42:56 -0300

    switch added

---
+++ app/switch.py
@@ -0,0 +1,24 @@
+
+class Switch:
+
+    def __init__(self) -> None:
+        
+        self.running = True
+
+        while self.running:
+            print(f'\n------------- Algoritmos -------------')
+
+            print('1 - Bagged Decision Tree..............')
+            print('0 - Exit Program......................')
+
+            print(f'--------------------------------------\n')
+
+            self.choice = int(input('Escolha uma opção: '))
+
+
+            match self.choice:
+                case 0:
+                    self.running = False
+                    print('Saindo...')
+                case 1:
+                    print('Bagged Decision Tree')
--- main.py
+++ main.py
@@ -1,25 +1,7 @@
-from app.bagged_decidion_tree import BaggedDecisionTree
+from app.switch import Switch
 
 def main():
+    Switch()
     
-    running = True
-
-    while running:
-        print(f'\n------------- Algoritmos -------------')
-
-        print('1 - Bagged Decision Tree..............')
-        print('0 - Exit Program......................')
-
-        print(f'--------------------------------------\n')
-
-        choice = int(input('Escolha uma opção: '))
-
-
-        match choice:
-            case 0:
-                running = False
-            case 1:
-                print('Bagged Decision Tree')
-
 if __name__ == '__main__':
     main()
