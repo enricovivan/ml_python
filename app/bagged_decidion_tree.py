ca037aecd94c1941823149688f035f427540f5a7
enricovivan <enricovivan21@gmail.com>
2023-01-19 17:54:36 -0300

    read csv added in bagged decision tree

--- app/bagged_decidion_tree.py
+++ app/bagged_decidion_tree.py
@@ -7,3 +7,8 @@ class BaggedDecisionTree:
     def __init__(self) -> None:
         
         print('Bagged Decision Tree')
+
+        # Importação do dataset
+        self.dataset = pd.read_csv('app/data/Breast_Cancer.csv')
+
+        
