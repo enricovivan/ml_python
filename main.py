7a9dad2a9daa11e8c0038904025f5d1cf6ff1b7e
enricovivan <enricovivan21@gmail.com>
2023-01-19 17:33:30 -0300

    menu prototype

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
+from app.bagged_decidion_tree import BaggedDecisionTree
+
 def main():
-    pass
+    
+    running = True
+
+    while running:
+        print(f'\n------------- Algoritmos -------------')
+
+        print('1 - Bagged Decision Tree..............')
+        print('0 - Exit Program......................')
+
+        print(f'--------------------------------------\n')
+
+        choice = int(input('Escolha uma opção: '))
+
+
+        match choice:
+            case 0:
+                running = False
+            case 1:
+                print('Bagged Decision Tree')
 
 if __name__ == '__main__':
     main()
