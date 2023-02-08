4dd52716b275e3d9c13bfa6707cd1c2875fbff0b
enricovivan <enricovivan21@gmail.com>
2023-02-07 21:21:23 -0300

    sns error

--- app/random_forest.py
+++ app/random_forest.py
@@ -2,19 +2,32 @@ import pandas as pd
 import numpy as np
 import sklearn.ensemble as sk
 from dataprep.eda import create_report
+from matplotlib import pyplot as plt
+import seaborn as sns
 
 class RandomForest:
 
     def __init__(self) -> None:
         
-        print('Bagged Decision Tree')
+        print('\nBagged Decision Tree\n')
 
         # Importação do Dataset
         self.data = pd.read_csv('app/data/Breast_Cancer.csv')
 
-        # Verificar Linhas Duplicadas
-        print(self.data.duplicated())
+        # Verificar Linhas Duplicadas e Valores Nulos
+        print(f'Quantidade de Dados Duplicados: \n{self.data.duplicated().sum()}\n')
+        print(f'Valores Nulos: \n{self.data.isnull().sum()}')
 
+        # Neste caso existe uma linha duplicada, faremos a remoção da mesma em um outro dataset
+        # Para isso, copiamos o original para futura referência
+
+        # Cópia do Dataset para alteração
+        self.ds = self.data.copy()
+
+        sns.countplot(data=self.ds, x='Race', hue='Status')
+        plt.show()
+
+        # Remoção da linha duplicada
         
 
         
